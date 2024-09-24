6069e8a97b28c714fec74fa6f9f7ea84c8462108
AbbyKuo <abbykuo8776@gmail.com>
2024-09-24 15:50:07 +1000

    added Pledge model, PledgeSerializer, ProjectDetailSerializer.

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -5,3 +5,11 @@ class ProjectSerializer(serializers.ModelSerializer):
   class Meta:
       model = apps.get_model('projects.Project')
       fields = '__all__'
+
+class PledgeSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = apps.get_model('projects.Pledge')
+      fields = '__all__'
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
