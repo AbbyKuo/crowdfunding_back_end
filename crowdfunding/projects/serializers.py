43c0853d6214a0a7c4336eb3899596653e775809
AbbyKuo <abbykuo8776@gmail.com>
2024-09-28 12:19:21 +1000

    moved the owner from PledgeSerializer to ProjectSerializer in projects/serializers.py and added owener to serializer.save() for ProjectList post method in projects/views.py

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from django.apps import apps
 
 class ProjectSerializer(serializers.ModelSerializer):
+  owner = serializers.ReadOnlyField(source='owner.id')
   class Meta:
       model = apps.get_model('projects.Project')
       fields = '__all__'
 
 class PledgeSerializer(serializers.ModelSerializer):
-   owner = serializers.ReadOnlyField(source='owner.id')
-   class Meta:
+  class Meta:
       model = apps.get_model('projects.Pledge')
       fields = '__all__'
 
