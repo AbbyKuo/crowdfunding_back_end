46f124be6525846f0e33f77741c3f4643fec74d8
AbbyKuo <abbykuo8776@gmail.com>
2024-09-28 12:49:00 +1000

    applyed new permission class to ProjectDetail

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 from django.http import Http404
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+
 
 class ProjectList(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -30,9 +32,15 @@ class ProjectList(APIView):
         )
     
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+    
     def get_object(self, pk):
         try:
             project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
             return project
         except Project.DoesNotExist:
             raise Http404
@@ -52,7 +60,7 @@ class ProjectDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    
+
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
