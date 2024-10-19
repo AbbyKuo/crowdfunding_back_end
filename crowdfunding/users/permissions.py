6d2b4c32d141bf0e7740e1a3f8e4d9c7d22d5411
AbbyKuo <abbykuo8776@gmail.com>
2024-10-19 10:25:46 +1000

    Removed .models import and allowed admin user to update user details

--- crowdfunding/users/permissions.py
+++ crowdfunding/users/permissions.py
@@ -1,12 +1,11 @@
 from rest_framework import permissions
-from .models import CustomUser
 
 class IsUserOrAdminOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            return obj == request.user or request.user.is_staff
+            return obj == request.user or obj == request.user.is_staff
         if request.method == "DELETE":
-            return obj == request.user or request.user.is_staff
+            return obj == request.user or obj == request.user.is_staff
         if request.method == "PUT":
             return obj == request.user 
         return False 
